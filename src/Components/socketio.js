875ddd7e8b3e20cf19b6473e19dc574c26df5f7c
Aditya Srivastava <adityasrivastava@Adityas-MacBook-Air.local>
2025-03-28 11:01:59 +0530

    gitCheck 2.0

--- src/Components/socketio.js
+++ src/Components/socketio.js
@@ -1,16 +1,7 @@
 import { io } from "socket.io-client";
-<<<<<<< HEAD
- 
- const socket = io("http://localhost:3000", {
-     transports: ["websocket"], // Ensures WebSocket connection
-   });
- 
- export default socket;
-=======
 
 const socket = io("http://localhost:3000", {
   transports: ["websocket"], // Ensures WebSocket connection
 });
 
 export default socket;
->>>>>>> b79846cdabc51a7b16f95c5a65aa775161263d11
--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -126,7 +126,7 @@ const LandingPage = () => {
           initial="hidden"
         >
           <StaircaseText
-            text="VAIDYAMITRA"
+            text="MyMitra"
             className="font-bold text-[#c94a4a] text-8xl mb-4 mt-5 space-x-2"
           />
         </motion.div>
